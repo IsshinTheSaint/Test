fd5a62faf5d4ec2e6e1b8fe4da1ffc075e7fb2d8
Isshin <144317757+IsshinTheSaint@users.noreply.github.com>
2024-11-04 09:26:48 +0100

    tgegg

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const inputt= document.getElementById("textik")
+const tlac = document.getElementById("tlacitko")
+const output = document.createElement("div")
+
+console.log(textik)
+console.log(tlacitko)
